bd5fb74bf335e9e63389198596ce05afc91fbe10
Mathias Larsen <mathiaslarsen1122@gmail.com>
2025-02-07 10:28:03 +0100

    added details.js

--- destinations.js
+++ destinations.js
@@ -9,18 +9,23 @@ fetch("/data/destinations.json")
     .then(data => {
         console.log(data);
         let destiSection = document.querySelector(".destination__choice");
-        let listElm = document.createElement("ul");
+        destiSection.classList.add("destination__choice--details");
+        destiSection.innerHTML = `<h1>Apartments</h1>`;
+        let divElm = document.createElement("div");
+        divElm.classList.add("destination__details");
 
         data.destinations.forEach(destination => {
-            let listItem = document.createElement("li");
-            listItem.innerHTML = `
-                <img src="${destination.image}">
-                <h3>${destination.title}</h3>
-                <p>${destination.subtitle}</p>
-                ${`<a href="details.html?id=${destination.id}">MORE</a>`}
+            let divItem = document.createElement("div");
+            divItem.classList.add("destination__details--item");
+            divItem.innerHTML = `
+                    <img class="main__img" src="${destination.image}">
+                <div class="destination__details--text">
+                    <img class="heart__img" src="${'img/heart.png'}" alt="">
+                    ${`<a href="details.html?id=${destination.id}">MORE</a>`}
+                </div>
             `;
-            listElm.appendChild(listItem);
+            divElm.append(divItem);
         });
 
-        destiSection.appendChild(listElm);
+        destiSection.append(divElm);
     });
